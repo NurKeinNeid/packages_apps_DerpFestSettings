0d7e80570d32878ba834c5524518a9186ee9ae3b
Erfan Abdi <erfangplus@gmail.com>
2023-12-10 13:22:36 +0330

    DerpFestSettings: Implement notificationlight settings
    Based on https://github.com/LineageOS/android_packages_apps_LineageParts
    
    [Updated to U by: ikeramat]
    ikeramat: taken and adapted from: https://github.com/LineageOS/android_packages_apps_LineageParts/commit/18f2dec2479692fd9e2b97d54acdfddf47d6150d
    
    Change-Id: I44457aa51377bc70a1fdb11dfa089a17732dfd85
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

---
+++ src/com/android/settings/derpfest/notificationlight/NotificationBrightnessPreference.java
@@ -0,0 +1,39 @@
+/*
+ * SPDX-FileCopyrightText: 2017-2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.derpfest.notificationlight;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.UserHandle;
+import android.util.AttributeSet;
+
+import org.derpfest.providers.DerpFestSettings;
+import android.provider.Settings;
+
+public class NotificationBrightnessPreference extends BrightnessPreference {
+    private static final String TAG = "NotificationBrightnessPreference";
+
+    private final ContentResolver mResolver;
+
+    public NotificationBrightnessPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mResolver = context.getContentResolver();
+    }
+
+    @Override
+    protected int getBrightnessSetting() {
+        return Settings.System.getIntForUser(mResolver,
+                DerpFestSettings.System.NOTIFICATION_LIGHT_BRIGHTNESS_LEVEL,
+                LIGHT_BRIGHTNESS_MAXIMUM, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    protected void setBrightnessSetting(int brightness) {
+        Settings.System.putIntForUser(mResolver,
+                DerpFestSettings.System.NOTIFICATION_LIGHT_BRIGHTNESS_LEVEL,
+                brightness, UserHandle.USER_CURRENT);
+    }
+}
